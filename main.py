20fd5fefed0c05ea06c9e28d004758a9aedba8ec
unsunnybeach <irinapuchkova08@gmail.com>
2023-05-15 09:33:25 +0200

    next one

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last: str
+
+class StudentUpdateSchema(BaseModel):
+    first_name: str
+    last_name: str
 
 app = FastAPI()
 
+students = [{"first_name": "str",
+         "last_name": "str"
+         }]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/students/")
+async def get_all_students():
+    return students
+
+@app.post("/students/", status_code=200)
+async def create_student(student: StudentCreateSchema):
+    students.append(student)
+    return student
+
+
+@app.get("/student/{student_id}")
+async def get_student(student_id: int):
+    return students[student_id]
+
+@app.put("/student/{student_id}")
+async def update_student(student_id: int,
+                         student: StudentUpdateSchema):
+    students[student_id] = student
+    return student
